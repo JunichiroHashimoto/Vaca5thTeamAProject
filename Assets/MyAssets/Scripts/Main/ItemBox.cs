aa124c7d90379ec6975dfe7577fca206ec2e3b01
j16 <j16.hashimoto@gmail.com>
2019-06-15 14:02:45 +0900

    コミットから外れていたファイルがあったのでコミットしなおし

---
+++ Assets/MyAssets/Scripts/Main/ItemBox.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ItemBox : MonoBehaviour {
+
+
+    [SerializeField]
+    GameObject nodePrefab;
+    [SerializeField]
+    Transform nodeParent;
+    [SerializeField]
+    GameObject showCase;
+
+    public void SetItemList(Dictionary<int, ItemBase> itemDict)
+    {
+        foreach (var item in itemDict)
+        {
+            // リストの親要素の下に複製
+            var node = Instantiate(nodePrefab, nodeParent);
+            // アイテムデータから名称、画像ファイル等設定
+            node.GetComponentInChildren<TextMeshProUGUI>().text = item.Value.jpName;
+            // アイテムID をファイル名.png としてResourcesフォルダのImagesフォルダに保存
+            node.GetComponent<Image>().sprite = Resources.Load("Images/" + item.Value.id.ToString(), typeof(Sprite)) as Sprite;
+            node.GetComponent<ItemBoxNode>().itemData = item.Value;
+        }
+    }
+
+    public void DisplayItem(GameObject itemObj)
+    {
+        foreach(Transform ch in showCase.transform)
+        {
+            Destroy(ch.gameObject);
+        }
+        itemObj.transform.SetParent(showCase.transform);
+        itemObj.transform.localPosition = Vector3.zero;
+
+    }
+}
---
+++ Assets/MyAssets/Scripts/Main/ItemBoxNode.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemBoxNode : MonoBehaviour {
+
+    public ItemBase itemData;
+    ItemBox itemBox;
+
+    void Start()
+    {
+        itemBox = GetComponentInParent<ItemBox>();
+    }
+
+    public void OnClick()
+    {
+        if (itemData == null)
+        {
+            Debug.Log("OnClick Node Item null");
+            return;
+        }
+        Debug.Log("OnClick Node");
+
+        var itemPrefab = Resources.Load("Prefabs/Prop/" + itemData.resourceName) as GameObject;
+        var item = Instantiate(itemPrefab) as GameObject;
+
+        var rigid = item.GetComponent<Rigidbody>();
+        if(rigid)
+        {
+            rigid.isKinematic = true;
+        }
+        //var collider = item.GetComponent<Collider>();
+        //if(collider)
+        //{
+        //    collider.enabled = false;
+        //}
+
+        itemBox.DisplayItem(item);
+
+    }
+}
